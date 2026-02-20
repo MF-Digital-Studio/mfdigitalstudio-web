ae1b62632b8351a8c88c5b5bcc6071c0d625ab44
farukozgu <faruk.ozgufb@gmail.com>
2026-02-21 00:19:25 +0300

    feat(analytics): add @vercel/analytics for improved tracking

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,7 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { ScrollToTop } from "./components/ScrollToTop";
 import logo from "./assets/logo.svg";
+import { Analytics } from "@vercel/analytics/next"
 
 function ScrollToHash() {
   const location = useLocation();
@@ -105,6 +106,7 @@ export default function App() {
         <Footer />
         <ScrollToTop />
       </div>
+      <Analytics />
     </BrowserRouter>
   );
 }
